0e094b0389e973f966afd2b604de6e89a7f078ee
Granville Barnett <granville.barnett@hazelcast.com>
2024-06-03 14:45:00 +0100

    hit all maps

--- src/main/kotlin/MapQueryRunner.kt
+++ src/main/kotlin/MapQueryRunner.kt
@@ -10,20 +10,22 @@ class MapQueryRunner(
     private val instance: HazelcastInstance, private val keepRunning: AtomicBoolean, private val counter: AtomicLong,
     private val slowOps: AtomicLong) : Runnable {
     override fun run() {
-        val map = instance.getMap<String,Int>("myMap")
         while (keepRunning.get()) {
-            for (i in 0 until 100) {
-                val start = Instant.now()
-                try {
-                    map.set("k$i", i);
-                    counter.incrementAndGet()
-                } catch (e: Exception) {
-                    println("Error: ${e.message}")
-                }
-                val stop = Instant.now()
-                val duration = Duration.between(start, stop).toMillis()
-                if (duration > 500) {
-                    slowOps.incrementAndGet()
+            for (mapSuffix in 1 .. 150) {
+                val map = instance.getMap<String,Int>("map$mapSuffix")
+                for (i in 0 until 100) {
+                    val start = Instant.now()
+                    try {
+                        map.set("k$i", i);
+                        counter.incrementAndGet()
+                    } catch (e: Exception) {
+                        println("Error: ${e.message}")
+                    }
+                    val stop = Instant.now()
+                    val duration = Duration.between(start, stop).toMillis()
+                    if (duration > 500) {
+                        slowOps.incrementAndGet()
+                    }
                 }
             }
         }
