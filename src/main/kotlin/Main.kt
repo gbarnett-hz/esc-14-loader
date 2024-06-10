752d1b3811ddd0b88e920111ba6d7808122b6c87
Granville Barnett <granville.barnett@hazelcast.com>
2024-06-10 16:41:45 +0100

    make things more customisable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,8 @@ package org.example
 
 import com.hazelcast.client.HazelcastClient
 import com.hazelcast.client.config.ClientConfig
+import java.util.Random
+import java.util.UUID
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
@@ -14,25 +16,27 @@ fun main(args: Array<String>) {
     cfg.networkConfig.setAddresses(ips)
     val client = HazelcastClient.newHazelcastClient(cfg)
 
-    val src = "map0"
+    val src = "map"
     when (cmd) {
         "seed" -> {
-            val map = client.getMap<String, String>(src)
-            val items = HashMap<String, String>()
-            val value =
-                "dfgshjfdfgshjfhjdfsgjkldfgkldfsjgdflkgjdfklgjdflkgjdflskgjdfsklgdjslgkdsfjgkdfglsgjghjghjghfjgjgjghjhjdfsgjkldfgkldfsjgdflkgjdfklgjdflkgjdflskgjdfsklgdjslgkdsfjgkdfglsgjghjghjghfjgjgjghj"
-            for (i in 0 until 100_000) {
-                items["k$i"] = value
+            val valueSizeBytes = Integer.parseInt(args[2])
+            val keys = Integer.parseInt(args[3])
+            val v = ByteArray(valueSizeBytes)
+            Random().nextBytes(v)
+            val map = client.getMap<String, ByteArray>(src)
+            val items = HashMap<String, ByteArray>()
+            for (i in 0 until keys) {
+                items["k$i"] = v
             }
             map.setAll(items)
         }
         "copy" -> {
+            val copies = Integer.parseInt(args[2])
             val counter = AtomicInteger()
             val fs = ArrayList<Future<*>>()
             val es = Executors.newFixedThreadPool(32)
-            val copies = 150
             for (i in 1 .. copies) {
-                fs.add(es.submit(Replicator(client, counter, src, "map$i")))
+                fs.add(es.submit(Replicator(client, counter, src, "${UUID.randomUUID()}")))
             }
 
             while (!fs.all { it.isDone }) {
@@ -48,6 +52,10 @@ fun main(args: Array<String>) {
             val threads = Integer.parseInt(args[3])
             val crm = ClientRequestMonitor(client, secondsToRun, threads)
             crm.run()
+        }
+        "map" -> {
+            val m = client.getMap<String, String>("map-native")
+            println(m.size)
         }
         else -> {
             println("Unknown command: $cmd")
--- src/main/kotlin/Replicator.kt
+++ src/main/kotlin/Replicator.kt
@@ -3,11 +3,14 @@ package org.example
 import com.hazelcast.core.HazelcastInstance
 import java.util.concurrent.atomic.AtomicInteger
 
-class Replicator(val client: HazelcastInstance, val counter: AtomicInteger, val src: String, val dest: String) : Runnable {
+class Replicator(private val client: HazelcastInstance,
+                 private val counter: AtomicInteger,
+                 private val src: String,
+                 private val dest: String) : Runnable {
     override fun run() {
-        val src = client.getMap<String, String>(src)
-        val map = client.getMap<String, String>(dest)
-        map.setAll(src)
+        val srcMap = client.getMap<String, ByteArray>(src)
+        val dstMap = client.getMap<String, ByteArray>(dest)
+        dstMap.setAll(srcMap)
         counter.incrementAndGet()
     }
 }
